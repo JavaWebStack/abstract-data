e3de5aab40b128614b5ee310e74b76d1094ce014
Jake Esser <j.esser@lumaserv.com>
2023-11-09 13:37:30 +0100

    Fxied DateFormat annotation issue

--- src/main/java/org/javawebstack/abstractdata/mapper/annotation/DateFormat.java
+++ src/main/java/org/javawebstack/abstractdata/mapper/annotation/DateFormat.java
@@ -1,5 +1,12 @@
 package org.javawebstack.abstractdata.mapper.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DateFormat {
 
     String value() default "";
